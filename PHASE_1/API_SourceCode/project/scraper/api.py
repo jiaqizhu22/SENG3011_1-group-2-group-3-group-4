14f9c39ed9c82eadfeb59c144b7c682a874c9c87
Joel Ivory <j.ivory@student.unsw.edu.au>
2022-03-15 12:29:15 +1100

    Fixed bugs on api class.

--- PHASE_1/API_SourceCode/project/scraper/api.py
+++ PHASE_1/API_SourceCode/project/scraper/api.py
@@ -37,45 +37,43 @@ class Api:
         self.report = data['report']
         self.log = data['log']
 
+# article_list = []
+# with open('PHASE_1/API_SourceCode/project/scraper/data.json', 'r') as json_file:
+#     # Store the json data as a list
+#     data = json.load(json_file)
 
-
-article_list = []
-with open('PHASE_1/API_SourceCode/project/scraper/data.json', 'r') as json_file:
-    # Store the json data as a list
-    data = json.load(json_file)
-
-    for item in data:
-        url = item['url']
-        pub_date = item['date_of_publication']
-        headline = item['headline']
-        content = item['main_text']
-        reports_list = []
-        reports = item['reports']
-        print(reports)
-        for r in reports.split(''):
-            print(r)
-            diseases = r[0]
-            syndromes = r[1]
-            event_date = r[2]
-            locs_list = r[3]
-            report = {
-                "diseases": diseases,
-                "syndromes": syndromes,
-                "event_date":"", #this needs to be sorted out
-                "locations":locs_list,  
-            }
-            reports.append(report)
+#     for item in data:
+#         url = item['url']
+#         pub_date = item['date_of_publication']
+#         headline = item['headline']
+#         content = item['main_text']
+#         reports_list = []
+#         reports = item['reports']
+#         print(reports)
+#         for r in reports.split(''):
+#             print(r)
+#             diseases = r[0]
+#             syndromes = r[1]
+#             event_date = r[2]
+#             locs_list = r[3]
+#             report = {
+#                 "diseases": diseases,
+#                 "syndromes": syndromes,
+#                 "event_date":"", #this needs to be sorted out
+#                 "locations":locs_list,  
+#             }
+#             reports.append(report)
         
-        article = { #making dict of the info.
-            "url":url,
-            "date_of_publication": date,
-            "headline": headline,
-            "main_text":main_text,
-            "reports":reports,
-        }
-        article_list.append(article)
+#         article = { #making dict of the info.
+#             "url":url,
+#             "date_of_publication": date,
+#             "headline": headline,
+#             "main_text":main_text,
+#             "reports":reports,
+#         }
+#         article_list.append(article)
 
-    # --- Handling different requests ---
-    # Request all reports in a country
+#     # --- Handling different requests ---
+#     # Request all reports in a country
 
-print(article_list)
+# print(article_list)
