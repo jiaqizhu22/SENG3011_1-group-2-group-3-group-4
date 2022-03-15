0097d747ab217984731487d187aee907142435a4
Joel Ivory <j.ivory@student.unsw.edu.au>
2022-03-15 12:15:51 +1100

    Created api class to be utilised for d2.

--- PHASE_1/API_SourceCode/project/scraper/api.py
+++ PHASE_1/API_SourceCode/project/scraper/api.py
@@ -1,8 +1,46 @@
 import json
 
-article_list = []
+class Api:
+    
+    def __init__(self,article,report,log):
+        self.article = {
+            "url":url,
+            "date_of_publication": date,
+            "headline": headline,
+            "main_text":main_text,
+            "reports":reports,
+        }
+        self.report = {
+            "diseases": diseases,
+            "syndromes": syndromes,
+            "event_date":"", #this needs to be sorted out
+            "locations":locs_list,
+        }
+        self.log = {
+            "Team Name": team,
+            "Access Time": datetime,
+            "Data Source": source_url
+        }
 
-with open('PHASE_1/API_SourceCode/project/scraper/data.json') as json_file:
+    # If we want to save our api class to a JSON file for other teams to use
+    def save_to_json(self,filename):
+        api_dict = {'article': self.article, 'report': self.report, 'log': self.log}
+        with open(filename, 'w') as f:
+            f.write(json.dumps(api_dict, indent=4))
+
+    # Loading our api class from a json file
+    def load_from_json(self, filename):
+        with open(filename, 'r') as f:
+            data = json.loads(f.read())
+        #Assigning components
+        self.article = data['article']
+        self.report = data['report']
+        self.log = data['log']
+
+
+
+article_list = []
+with open('PHASE_1/API_SourceCode/project/scraper/data.json', 'r') as json_file:
     # Store the json data as a list
     data = json.load(json_file)
 
