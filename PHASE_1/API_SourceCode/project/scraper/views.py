35e9107a7c9b2018e76b06859316d329a738c6b6
Joel Ivory <j.ivory@student.unsw.edu.au>
2022-03-15 14:40:40 +1100

    Set up basic version of backend homepage, with a link to the swagger documentation page

--- PHASE_1/API_SourceCode/project/scraper/views.py
+++ PHASE_1/API_SourceCode/project/scraper/views.py
@@ -1,5 +1,30 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+# Create your views here.
+
+#from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse("This is the index page for the scraper API.")
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    #num_books = Book.objects.all().count()
+    #num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    #num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    #num_authors = Author.objects.count()
+
+    #context = {
+        # 'num_books': num_books,
+        # 'num_instances': num_instances,
+        # 'num_instances_available': num_instances_available,
+        # 'num_authors': num_authors,
+    #}
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html')
+
