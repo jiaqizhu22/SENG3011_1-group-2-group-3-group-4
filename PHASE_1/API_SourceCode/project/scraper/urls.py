851918e2972b24e585955f22e47ea889a0872f79
Smae De <mitrblue@gmail.com>
2022-03-15 17:37:58 +1100

    Make fake search endpoint and update swagger

--- PHASE_1/API_SourceCode/project/scraper/urls.py
+++ PHASE_1/API_SourceCode/project/scraper/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
     path('docs/', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url': 'openapi-schema'}
