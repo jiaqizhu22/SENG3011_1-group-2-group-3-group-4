ca4393fb0f829a452b0961eece2e6e7f228fd53f
Jiaqi <blacker0lily311@gmail.com>
2022-03-17 15:50:42 +1100

    resolve merge issues

---
+++ PHASE_1/API_SourceCode/project/scraper/diseases.py
@@ -0,0 +1,24 @@
+import json
+
+# id: serial
+# name: string
+
+class Diseases:
+    
+    def __init__(self,disease_id,name):
+        self.disease_id = disease_id # serial
+        self.name = name # string
+
+    # If we want to save our api class to a JSON file for other teams to use
+    def save_to_json(self,filename):
+        api_dict = {'disease_id':self.disease_id, 'name':self.name}
+        with open(filename, 'w') as f:
+            f.write(json.dumps(api_dict, indent=4))
+
+    # Loading our api class from a json file
+    def load_from_json(self, filename):
+        with open(filename, 'r') as f:
+            data = json.loads(f.read())
+        # Assigning components
+        self.disease_id = data['disease_id'] # serial
+        self.name = data['name'] # string
