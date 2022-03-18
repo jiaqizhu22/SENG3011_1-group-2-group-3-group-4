40cb6d8faf2caaedf66d20741eb4e8260724b3f8
Jiaqi <blacker0lily311@gmail.com>
2022-03-18 18:11:00 +1100

    Updated /search endpoint with new filters and now response body is correct for most cases

--- PHASE_1/API_SourceCode/scraper/views.py
+++ PHASE_1/API_SourceCode/scraper/views.py
@@ -7,9 +7,6 @@ from .models import Articles, Reports, Locations
 from django.utils.timezone import make_aware
 from datetime import datetime
 
-# Create your views here.
-
-#from .models import Book, Author, BookInstance, Genre
 
 def index(request: HttpRequest):
     """View function for home page of site."""
@@ -53,59 +50,74 @@ def search(request: HttpRequest):
         key_terms = map(lambda x : x.lower(), key_terms)
     else:
         key_terms = []
-    
+        
     # Load scraper to get data - list of dicts
     # data = web_scraper()
     
     # Search results - articles matching parameters
     results = []
 
+    # First filter: find articles published in requested period
     articles = Articles.objects.filter(date_of_publication__range=(start_date, end_date))
+    
     for article in articles:
-        article_string = article.__str__()
-        '''
-        {
-            'url': self.url,
-            'date_of_publication': self.date_of_publication,
-            'headline': self.headline,
-            'main_text': self.main_text,
+        article_dict = article.__dict__
+        article_string = {
+            'url': article_dict['url'],
+            'date_of_publication': article_dict['date_of_publication'],
+            'headline': article_dict['headline'],
+            'main_text': article_dict['main_text'],
+            'reports': []
         }
-        '''
+        
+        # Find all reports in this article
         report_list = []
         reports = Reports.objects.filter(parent_id=article)
         for report in reports:
-            report_string = report.__str__()
+            # Return report as a dict in the following format
+            report_string = report.__dict__
+            # print(report_string)
             '''
-            {
-                'diseases': self.diseases,
-                'syndromes': self.syndromes,
-                'event_date': self.event_date,
-                'locations': self.locations,
-                'report_id': self.id,
-                'parent_id': self.parent_id
-            }
+            {'_state': <django.db.models.base.ModelState object at 0x00000212364E2DC0>,
+            'id': 2551, 
+            'parent_id_id': 2651, 
+            'event_date': datetime.datetime(1996, 1, 22, 0, 0, tzinfo=datetime.timezone.utc), 
+            'diseases': 'Global Cholera Update', 
+            'syndromes': None, 
+            'locations_id': 29}
             '''
             
             match_location = False
             # Find location
-            location_id = report_string['locations']
-            # Error: location shouldn't all be None
-            print(location_id)
+            location_id = report_string['locations_id']           
             location_string = {
                 'country': '',
                 'location': ''
             }
             if location_id != None:
                 stored_location = Locations.objects.get(id=location_id)
-                location_string = stored_location.__str__()
-                print(location_string)
+                temp_location_string = stored_location.__dict__
+                location_string['country'] = temp_location_string['country']
+                location_string['location'] = temp_location_string['location']
+                
             # Check if location matches 
-            if location == None or location.lower() == location_string['country'].lower() or location.lower() == location_string['location'].lower():
+            if key_terms == [] and location is None:
                 match_location = True
+                # print('Location matched')
+            elif location is not None:
+                if location_string['country'] is not None and location.lower() == location_string['country'].lower():
+                    match_location = True
+                if location_string['location'] is not None and location.lower() == location_string['location'].lower():
+                    match_location = True
                 
             match_key_terms = False       
-            if key_terms == [] or report_string['diseases'].lower() in key_terms or report_string['syndromes'].lower() in key_terms:
+            if key_terms == [] and location is None:
                 match_key_terms = True
+            elif key_terms != []:
+                if report_string['diseases'] is not None and report_string['diseases'].lower() in key_terms:
+                    match_key_terms = True
+                if report_string['syndromes'] is not None and report_string['syndromes'].lower() in key_terms:
+                    match_key_terms = True
 
             if match_location is True or match_key_terms is True:
                 report_info = {
@@ -123,15 +135,12 @@ def search(request: HttpRequest):
                     report_info['event_date'] = report_string['event_date'].strftime("%Y-%m-%dT%H-%M-%S")
                 if location_string != {'country': '','location': ''}:
                     report_info['locations'].append(location_string)
-                    
                 report_list.append(report_info)
-            else:
-                continue
-
-        article_string['reports'] = report_list
-        results.append(article_string)
+        # Only add article to result list if there are matching reports
+        if report_list != []:
+            article_string['reports'] = report_list
+            results.append(article_string)
         
-    
     return JsonResponse({
         "articles": results
     })
