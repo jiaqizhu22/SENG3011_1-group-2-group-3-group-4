34a3141528056807d99af0b13fb4c65997256308
wrwan <william.wan02@gmail.com>
2022-04-03 21:48:25 +1000

    made header, added date range picker, add forms for location and key terms

--- PHASE_2/frontend/src/components/searchBar/searchBar.jsx
+++ PHASE_2/frontend/src/components/searchBar/searchBar.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './searchBar.css';
+import {useState} from 'react'
+import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
+
 
 const SearchBar = () => (
     <div className="searchBar">
-      fine
+      <div class="container">
+        <div class="column">
+          <DateRangePickerComponent placeholder="Enter date range: "></DateRangePickerComponent>
+        </div>
+        <div class="column">
+          <form>
+            <label> 
+              <input placeholder="Location" type="text" name="location" style={{width: "370px"}}/>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div class="column">
+
+        <form>
+            <label>
+              <input placeholder="Key terms seperated by commas" type="text" name="keyterms" style={{width: "370px"}}/>
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 export default SearchBar;
