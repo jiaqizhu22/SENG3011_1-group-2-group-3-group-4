9487a07926dc1440c416b836fa71990e553d0289
hoshang <hoshangmehta111@gmail.com>
2022-04-03 20:21:08 +1000

    Add new logo

--- PHASE_2/frontend/src/App.js
+++ PHASE_2/frontend/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import Header from './components/header/header.jsx'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         
+      App 1 App 2 App 3
       </header>
     </div>
   );
---
+++ PHASE_2/frontend/src/components/header/header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './header.css';
+import logos from './unswLogo.png';
+
+const Header = () => (
+    <div className="header">
+      <img src={logos} alt="Group 1 Group 2 Group 3 Group 4" />
+    </div>
+  )
+export default Header;
