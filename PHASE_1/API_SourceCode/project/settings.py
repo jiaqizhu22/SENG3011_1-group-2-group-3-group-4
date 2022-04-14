1ccb1faa68f294dd0b78553fe78c32ec42d05244
Jiaqi <blacker0lily311@gmail.com>
2022-04-14 17:08:24 +1000

    Resolved bug with merge conflict and poetry install; attempt to connect mysql

--- PHASE_1/API_SourceCode/project/settings.py
+++ PHASE_1/API_SourceCode/project/settings.py
@@ -74,8 +74,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf',
+        }
     }
 }
 
