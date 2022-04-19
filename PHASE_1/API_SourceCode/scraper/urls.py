085e3cb9f50c29e308a3a1e950a99779eb411eb0
Smae De <mitrblue@gmail.com>
2022-04-20 01:19:59 +1000

    Update views to use new travel info

--- PHASE_1/API_SourceCode/scraper/urls.py
+++ PHASE_1/API_SourceCode/scraper/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
+    path('travelinfo/', views.travelinfo, name='travelinfo'),
     path('docs/', TemplateView.as_view(
         template_name='swagger-ui.html',
         extra_context={'schema_url': 'openapi-schema'}
